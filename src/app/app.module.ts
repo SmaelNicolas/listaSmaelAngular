2ee4a55e1ba83003c839a09e381bffc243c29b35
Smael Nicolas <nico.smael@gmail.com>
2022-03-24 21:34:21 -0300

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
 import { OrganizacionComponent } from './components/organizacion/organizacion.component';
+import { TituloComponent } from './components/titulo/titulo.component';
 
 @NgModule({
-  declarations: [AppComponent, ListaAlumnosComponent, OrganizacionComponent],
+  declarations: [AppComponent, ListaAlumnosComponent, OrganizacionComponent, TituloComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
