94af0eb0091a5c12aff0fec5d04da5b520218ddc
Smael Nicolas <nico.smael@gmail.com>
2022-03-24 21:07:14 -0300

    *ngFor , *ngIf , ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { OrganizacionComponent } from './components/organizacion/organizacion.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListaAlumnosComponent, OrganizacionComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
